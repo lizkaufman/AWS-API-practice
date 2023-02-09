692fe192ed9d03e55c488b681255839bf6b9c8ba
lizkaufman <lizkaufman92@gmail.com>
2023-02-09 14:12:07 +0000

    fixed app.js to remove authors

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const bookRouter = require("./routes/books.js");
-const authorRouter = require("./routes/authors.js");
+//const authorRouter = require("./routes/authors.js");
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/api/books", bookRouter);
-app.use("/api/authors", authorRouter);
+//app.use("/api/authors", authorRouter);
 
 app.listen(PORT, function () {
   console.log(`Server listening on port ${PORT}`);
